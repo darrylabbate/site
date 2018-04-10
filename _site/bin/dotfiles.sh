54520c004aaf42ff3963e11808a253eacfcd9bdc
Darryl Abbate <rootbeersoup@gmail.com>
2018-04-09 20:53:18 -0500

    Push new build

--- _site/bin/dotfiles.sh
+++ _site/bin/dotfiles.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-do
-	read -p "Are you sure? [y/N]" prompt
-	if [[ $prompt == "y" || $prompt == "Y" ]]; then
-		echo "Do the thing"
-	elif [[ $prompt == "n" || $prompt == "N" ]]; then
-		echo "fine whatevs"
-	else
-		echo "Choose yes or no"
-	fi
-done
+echo "Hello world"
